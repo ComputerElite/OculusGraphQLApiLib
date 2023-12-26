e8b59d2f2833c495433f12bddd3ac0d43ef8dbc3
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-12-26 23:17:05 +0100

    add more genres

--- OculusGraphQLApiLib/Results/Genre.cs
+++ OculusGraphQLApiLib/Results/Genre.cs
@@ -2,14 +2,18 @@ namespace OculusGraphQLApiLib.Results;
 
 public enum Genre
 {
-    CASUAL,
-    MUSIC,
-    SPORTS,
-    ART_CREATIVITY,
-    EDUCATIONAL,
-    ARCADE,
-    ACTION,
-    RELAXATION_MEDITATION,
-    MUSIC_VIDEO,
-    CANCELLED
+    CASUAL = 0,
+    MUSIC = 1,
+    SPORTS = 2,
+    ART_CREATIVITY = 3,
+    EDUCATIONAL = 4,
+    ARCADE = 5,
+    ACTION = 6,
+    RELAXATION_MEDITATION = 7,
+    MUSIC_VIDEO = 8,
+    CANCELLED = 9,
+    STRATEGY = 10,
+    ADVENTURE = 11,
+    EXPLORATION = 12,
+    DEGREE_360 = 13,
 }
