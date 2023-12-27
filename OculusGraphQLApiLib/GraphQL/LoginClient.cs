ee2eab338cfff5c5a5c49c8cf424c180e1f2210b
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-12-27 21:53:31 +0100

    Add loginclient

---
+++ OculusGraphQLApiLib/GraphQL/LoginClient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using ComputerUtils.Logging;
+using Microsoft.Win32;
+
+namespace OculusGraphQLApiLib.GraphQL;
+
+public class LoginClient
+{
+    public string etoken { get; set; } = "";
+    public string token { get; set; } = "";
+    public string blob { get; set; } = "";
+    public const string webviewTokensQuery = "https://meta.graph.meta.com//webview_tokens_query";
+    public const string webviewTokensDecrypt = "https://meta.graph.meta.com//webview_blobs_decrypt";
+    
+    
+    
+    /// <summary>
+    /// Starts the login process
+    /// </summary>
+    /// <returns></returns>
+    public LoginResponse StartLogin()
+    {
+        //RegisterUri();
+        LoginPayload payload = new LoginPayload(GraphQLClient.oculusClientToken);
+        // post payload to webviewTokensQuery url with httpclient
+        string response = DoPostRequest(webviewTokensQuery, JsonSerializer.Serialize(payload));
+        // parse response
+        LoginResponse loginResponse = JsonSerializer.Deserialize<LoginResponse>(response);
+        // set etoken
+        etoken = loginResponse.native_sso_etoken;
+        token = loginResponse.native_sso_token;
+        // return url
+        return loginResponse;
+    }
+
+    public string GetToken()
+    {
+        string response = DoPostRequest(webviewTokensDecrypt, JsonSerializer.Serialize(new LoginDecryptPayload
+        {
+            access_token = GraphQLClient.oculusClientToken,
+            blob = blob,
+            request_token = token
+        }));
+        return JsonSerializer.Deserialize<LoginDecryptResponse>(response).access_token;
+    }
+
+    public string DoPostRequest(string uri, string requestBody)
+    {
+        string responseString = "";
+        Console.WriteLine("body: " + requestBody);
+        using (HttpClient client = new HttpClient())
+        {
+            // Create StringContent with the request body and specify the media type
+            StringContent content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            try
+            {
+                // Send the POST request
+                HttpResponseMessage response = client.PostAsync(uri, content).Result;
+
+                // Check if the request was successful (status code 200-299)
+                responseString = response.Content.ReadAsStringAsync().Result;
+                Logger.Log(response.StatusCode.ToString());
+                Console.WriteLine("response: " + responseString);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read and display the response content as a string
+                }
+                else
+                {
+                    // If the request was not successful, display the status code
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, such as network issues
+                Console.WriteLine("Exception: " + ex);
+            }
+        }
+
+        return responseString;
+    }
+
+    public string UriCallback(LoginApproveResponse response)
+    {
+        string[] parameters = response.payload.uri.Replace("oculus://", "").Split('?')[1].Split('&');
+        string token = parameters[0].Split('=')[1];
+        string blob = parameters[1].Split('=')[1];
+        Console.WriteLine("token: " + token);
+        Console.WriteLine("blob: " + blob);
+        this.blob = blob;
+        this.token = token;
+        return GetToken();
+    }
+}
+
+public class LoginApproveRequest
+{
+    public string native_app_id { get; set; } = "";
+    public string native_sso_etoken { get; set; } = "";
+}
+public class LoginApproveResponse
+{
+    public LoginApproveResponsePayload payload { get; set; } = new LoginApproveResponsePayload();
+}
+public class LoginApproveResponsePayload
+{
+    public string uri { get; set; } = "";
+}
+
+public class LoginPayload
+{
+    public string access_token { get; set; } = "";
+    public LoginPayload(string clientToken)
+    {
+        access_token = clientToken;
+    }
+}
+
+public class LoginDecryptPayload
+{
+    public string access_token { get; set; } = "";
+    public string blob { get; set; } = "";
+    public string request_token { get; set; } = "";
+}
+
+public class LoginDecryptResponse
+{
+    public string access_token { get; set; } = "";
+    public long frl_account_id { get; set; } = 0;
+}
+
+public class LoginResponse
+{
+    public string native_sso_etoken { get; set; } = "";
+    public string native_sso_token { get; set; } = "";
+
+    public string url
+    {
+        get
+        {
+            return "https://auth.meta.com/native_sso/confirm?native_app_id=512466987071624&native_sso_etoken=" + native_sso_etoken +
+                   "&utm_source=skyline_splash";
+        }
+    }
+}
